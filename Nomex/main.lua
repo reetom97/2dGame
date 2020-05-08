2ca8e1131f2544313fdbd734b2ddccac4e1a1a7c
Reetom Rahman <reetom97@my.yorku.ca>
2020-05-07 20:11:16 -0400

    Update main.lua

--- Nomex/main.lua
+++ Nomex/main.lua
@@ -1,4 +1,8 @@
 push = require 'push'
+Class = require 'class'
+
+--require 'Ballon'
+--require 'Mountain'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -8,6 +12,14 @@ VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('asset/background.png')
 local bush = love.graphics.newImage('asset/ground.png')
+
+local backgroundScroll = 0
+local backgroundScroll_SPEED = 20
+
+local bushScroll = 0
+local bushScroll_SPEED = 65
+
+local backgroundScroll_LOOPING_POINT = 413
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -23,11 +35,19 @@ function love.resize(w, h)
     push:resize(w, h)
 end
 
+function love.update(dt)
+    backgroundScroll = (backgroundScroll + backgroundScroll_SPEED * dt) 
+        % backgroundScroll_LOOPING_POINT
+
+   bushScroll = (bushScroll + bushScroll_SPEED * dt) 
+        % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
 
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(bush, 0, VIRTUAL_HEIGHT - 16)
+    love.graphics.draw(background, -backgroundScroll, 0)
+    love.graphics.draw(bush, -bushScroll, VIRTUAL_HEIGHT - 16)
     
     push:finish()
 end
