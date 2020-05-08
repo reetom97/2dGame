73de6b5988419c0341c73ccd54843ac537f1dca5
Reetom Rahman <reetom97@my.yorku.ca>
2020-05-07 21:28:56 -0400

    changes bird position

--- Nomex/Ballon.lua
+++ Nomex/Ballon.lua
@@ -3,20 +3,12 @@ Ballon = Class {}
 local GRAVITY = 10
 
 function Ballon:init()
-    self.image = love.graphics.newImage("ballon.png")
-    self.x = VIRTUAL_WIDTH / 2 - 8
-    self.y = VIRTUAL_HEIGHT / 2 - 8
-
+    self.image = love.graphics.newImage("asset/ballon.png")
     self.width = self.image:getWidth()
     self.height = self.image:getHeight()
 
-    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.x = VIRTUAL_WIDTH / 4 - (self.width / 2)
     self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
-end
-
-function love.draw()
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.circle("fill", 300, 300, 50, 100) -- Draw white circle with 100 segments.
 end
 
 function Ballon:render()
