b638e12130e561a08d00a2c1d3bc6e3ca3c35d08
A29-S <adr29ian@gmail.com>
2020-05-07 21:26:27 -0400

    Update Ballon.lua
    function update to give gravity and velocity

--- Nomex/Ballon.lua
+++ Nomex/Ballon.lua
@@ -3,7 +3,7 @@ Ballon = Class {}
 local GRAVITY = 10
 
 function Ballon:init()
-    self.image = love.graphics.newImage("ballon.png")
+    self.image = love.graphics.newImage("asset/ballon.png")
     self.x = VIRTUAL_WIDTH / 2 - 8
     self.y = VIRTUAL_HEIGHT / 2 - 8
 
@@ -17,6 +17,16 @@ end
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.circle("fill", 300, 300, 50, 100) -- Draw white circle with 100 segments.
+end
+
+
+function Ballon:update(dt)
+    --appply gravity to velocity 
+
+    self.dy = self.dy + GRAVITY * dt
+
+    -- apply current velocity to Y position
+    self.y = self.y + self.dy 
 end
 
 function Ballon:render()
