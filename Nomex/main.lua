f8a59a72e6e967de955198b1d3914a407a53078b
A29-S <adr29ian@gmail.com>
2020-05-13 00:32:17 -0400

    re-did states

--- Nomex/main.lua
+++ Nomex/main.lua
@@ -9,8 +9,8 @@ require 'Cloud'
 require 'Bush'
 
 require 'StateMachine'
---require 'states/PlayState'
---require 'states/TitleScreenState'
+require 'states/PlayState'
+require 'states/TitleScreenState'
 --require 'states/BaseState'
 
 WINDOW_WIDTH = 1280
@@ -30,12 +30,6 @@ local bushScroll_SPEED = 50
 
 local backgroundScroll_LOOPING_POINT = 413
 
-local ballon = Ballon()
-
-local mountains = {}
-
-local spawn_Mountain_Timer = 0
-
 local scrolling = true
 
 function love.load()
@@ -43,14 +37,26 @@ function love.load()
 
     math.randomseed(os.time())
 
-    love.window.setTitle('Nomex')
+	love.window.setTitle('Nomex')
+	
+	smallFont = love.graphics.newFont('font.ttf', 8)
+    mediumFont = love.graphics.newFont('flappy.ttf', 14)
+    flappyFont = love.graphics.newFont('flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('flappy.ttf', 56)
+    love.graphics.setFont(flappyFont)
 
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = true
-    })
+	})
+	
+	gStateMachine = StateMachine {
+        ['title'] = function() return TitleScreenState() end,
+        ['play'] = function() return PlayState() end,
+    }
+    gStateMachine:change('title')
 
 
     love.keyboard.keysPressed = {}
@@ -81,50 +87,14 @@ function love.update(dt)
 	   bushScroll = (bushScroll + bushScroll_SPEED * dt) 
 	        % VIRTUAL_WIDTH
 
-	   spawn_Mountain_Timer = spawn_Mountain_Timer + dt
-
-	   if spawn_Mountain_Timer > 2 then
-	   		table.insert(mountains, Mountain())
-	   		spawn_Mountain_Timer = 0
-	   end
-
-	   ballon:update(dt)
-
-	   for i, mountain in pairs(mountains) do
-	   		mountain:update(dt)
-
-	   		if ballon:collides(mountain) then
-	   			scrolling = false
-	   		end	
-
-	   		if mountain.x < -mountain.width then
-	   			table.remove(mountains, i)
-	   		end
-	   end
-
-	   for i, mountain in pairs(mountains) do
-	   		if mountain.remove then
-	   			table.remove(mountain, i)
-	   		end
-	   end
-
-	   love.keyboard.keysPressed = {}
-
-   end
 end
 
 function love.draw()
     push:start()
 
     love.graphics.draw(background, -backgroundScroll, 0)
-
-    for i, mountain in pairs(mountains) do
-    	mountain:render()
-    end
-   
+	gStateMachine:render()
     love.graphics.draw(bush, -bushScroll, VIRTUAL_HEIGHT - 16)
-
-    ballon:render()
     
     push:finish()
 end
